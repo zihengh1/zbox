06a2c1771ef459d56d8e936e4c86b0f7b4cc54c0
zihengh1 <105703031@nccu.edu.tw>
2019-05-23 06:59:55 +0100

    test OTA

--- reboot.sh
+++ reboot.sh
@@ -4,9 +4,4 @@ sleep 5
 [ -f /home/pi/AirBox2/sense.py ] && {
     /usr/bin/sudo git -C /home/pi/AirBox2 fetch origin
     /usr/bin/sudo git -C /home/pi/AirBox2 reset --hard origin/master
-    /usr/bin/python /home/pi/AirBox2/sense.py > /home/pi/AirBox2/ans.txt
-    echo ok1 > /home/pi/AirBox2/config.txt
-} || {
-    /usr/bin/sudo git clone https://github.com/zihengh1/AirBox2.git
-    echo ok2 > /home/pi/AirBox2/config.txt
 }
