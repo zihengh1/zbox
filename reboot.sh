f19facc17b2c96fada9945f049420fb2538cf3cd
zihengh1 <105703031@nccu.edu.tw>
2019-05-23 04:34:27 +0100

    test OTA

--- reboot.sh
+++ reboot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-OTA="https://github.com/zihengh1/AirBox2.0.git"
+OTA="https://github.com/zihengh1/AirBox2/"
 
 sleep 5 
-[ -f /home/pi/AirBox2.0/main_sense.py ] && {
-    /usr/bin/sudo git -C /home/pi/AirBox2.0 fetch --all
-    /usr/bin/sudo git -C /home/pi/AirBox2.0 reset --hard origin/master
-    /usr/bin/python /home/pi/AirBox2.0/main_sense.py > /home/pi/AirBox2.0/ans.txt
+[ -f /home/pi/AirBox2/sense.py ] && {
+    /usr/bin/sudo git -C /home/pi/AirBox2 fetch --all
+    /usr/bin/sudo git -C /home/pi/AirBox2 reset --hard origin/master
+    /usr/bin/python /home/pi/AirBox2/sense.py > /home/pi/AirBox2/ans.txt
 } || {
-    /usr/bin/sudo git clone OTA /home/pi/AirBox2.0
+    /usr/bin/sudo git clone OTA /home/pi/AirBox2
 }
