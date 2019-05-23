eccd04468fab83ce1e07171e71997b821fca62b7
zihengh1 <105703031@nccu.edu.tw>
2019-05-23 07:03:05 +0100

    delete reboot.sh

--- reboot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sleep 5 
-[ -f /home/pi/AirBox2/sense.py ] && {
-    /usr/bin/sudo git -C /home/pi/AirBox2 fetch origin
-    /usr/bin/sudo git -C /home/pi/AirBox2 reset --hard origin/master
-}
